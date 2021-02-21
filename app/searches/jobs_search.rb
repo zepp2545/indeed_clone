df253d3dc0668a4d2d8f02d65891f0142c10b6f2
konan2610 <konan.maruyama@archibase.co.jp>
2021-02-21 16:22:40 +0900

    Fix when to join prefecture

--- app/searches/jobs_search.rb
+++ app/searches/jobs_search.rb
@@ -10,9 +10,9 @@ class JobsSearch
   ARRAY_FIELDS.each {|f| attribute f, :string }
 
   def search
-    jobs = Job.all.joins(:prefecture)
+    jobs = Job.all
     jobs = Job.where('title LIKE ? OR description LIKE ?', keyword, keyword) if keyword.present?
-    jobs = jobs.where('jobs.location_detail LIKE ? OR prefectures.name LIKE ?', "%#{location}%",  "#{location}%") if location.present?
+    jobs = jobs.joins(:prefecture).where('jobs.location_detail LIKE ? OR prefectures.name LIKE ?', "%#{location}%", "#{location}%") if location.present?
     jobs
   end
 
